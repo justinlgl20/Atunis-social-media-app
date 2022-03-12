059576c4eb2e92bdb69233036abd7334b33daaa4
Justin Goh <1020880@student.ccgs.wa.edu.au>
2022-03-12 16:48:55 +0800

    Fixed bug

--- media.py
+++ media.py
@@ -53,6 +53,9 @@ def profile(usr):
             db.session.commit()
             return redirect(url_for("media.profile", usr=usr.name))
     else:
+        if len(users.query.filter_by(name=usr)) == 0:
+            flash("No user found")
+            return redirect(url_for("media.home"))
         usr = users.query.filter_by(name=usr).first()
         followers = usr.followers.all()
         following = usr.followed.all()
